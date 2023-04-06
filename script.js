50e910e64690dac1bda1c87bd3108574f8ef536a
Kalombo john <johnkalombofr@gmail.com>
2023-04-06 21:12:13 +0100

    Fondamentatl Javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+/* Écrire un programme qui demande un mot de passe à l’utilisateur et qui affiche 
+"bienvenue" si le mot de passe est correct et "erreur de mot de passe" si le mot 
+de passe est incorrect.
+
+let motDePasse = prompt("Entrez votre mot de passe")
+
+if (motDePasse =="jkn202") {
+    console.log ("Bienvenue")
+} else {
+    console.log("erreur de mot de passe")
+}
+ */
